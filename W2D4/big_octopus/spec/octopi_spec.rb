3112ad1e03381d7aa1201c7b7aadf1be902bac63
brtsai <brtsai@ucsc.edu>
2018-02-21 19:51:50 -0800

    add rspec tests for nlogn and n^2 time

--- W2D4/big_octopus/spec/octopi_spec.rb
+++ W2D4/big_octopus/spec/octopi_spec.rb
@@ -31,6 +31,44 @@ def linear_runtime_with_sample?(n, &prc)
   proc_run_time < linear_run_time
 end
 
+def simulate_o_n_log_n_time(n)
+  fishes = get_fishes(n)
+
+  start_time = Time.now
+  fishes.sort
+  end_time = Time.now
+  
+  c = 2
+  c * (end_time - start_time)
+end
+
+def n_log_n_runtime_with_sample?(n, &prc)
+  n_log_n_run_time = simulate_o_n_log_n_time(n)
+  proc_run_time = runtime_with_n_fishes(n, &prc)
+  proc_run_time < n_log_n_run_time
+end
+
+def simulate_o_n_n_time(n)
+  fishes = get_fishes(n)
+
+  start_time = Time.now
+  fishes.each do |fish|
+    fishes.each do |other_fish|
+      fish.length == other_fish.length
+    end
+  end
+  end_time = Time.now
+  
+  c = 2
+  c * (end_time - start_time)
+end
+
+def n_n_runtime_with_sample?(n, &prc)
+  n_n_run_time = simulate_o_n_n_time(n)
+  proc_run_time = runtime_with_n_fishes(n, &prc)
+  proc_run_time < n_n_run_time
+end
+
 def runtime_with_n_fishes(n, &prc)
   fishes = get_fishes(n)
 
@@ -41,11 +79,11 @@ def runtime_with_n_fishes(n, &prc)
   end_time - start_time
 end
 
-
-
 describe "Octopi" do
   let (:fishes) { ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'] }
+
   describe "#clever_octopus" do
+
     it "finds the longest fish" do
       expect(clever_octopus(fishes)).to eq('fiiiissshhhhhh')
     end
@@ -70,6 +108,7 @@ describe "Octopi" do
         expect(linear_runtime_with_sample?(1000, &clever_proc)).to be(true)
       end
     end
+
   end
 
   describe "#dominant_octopus" do
@@ -77,69 +116,54 @@ describe "Octopi" do
     it "finds the longest fish" do
       expect(dominant_octopus(fishes)).to eq('fiiiissshhhhhh')
     end
-    context "for a small set of fishes" do
-      it "finds the longest fish in O(n) time" do
-        start_time = Time.now
-        dummy = "0"
-        fishes.each { |fish| fish == dummy }
-        end_time = Time.now
-
-        c = 2
-        linear_run_time = c * (end_time - start_time)
-        
-        start_time = Time.now
-        clever_octopus(fishes)
-        end_time = Time.now
-        clever_run_time = end_time - start_time
-
-        expect(clever_run_time).to be < linear_run_time
+    context "for 10 fishes" do
+      it "finds the longest fish in O(nlog(n)) time" do
+        dom_proc = method(:dominant_octopus)
+        expect(n_log_n_runtime_with_sample?(10, &dom_proc)).to be(true)
       end
     end
 
     context "for a hundred fishes" do
-      it "finds the longest fish in O(n) time" do
-        hundred_fishes = get_fishes(100)
-        start_time = Time.now
-        dummy = "0"
-        hundred_fishes.each { |fish| fish == dummy }
-        end_time = Time.now
-
-        c = 2
-        linear_run_time = c * (end_time - start_time)
-        
-        start_time = Time.now
-        clever_octopus(hundred_fishes)
-        end_time = Time.now
-        clever_run_time = end_time - start_time
-
-        expect(clever_run_time).to be < linear_run_time
+      it "finds the longest fish in O(nlog(n)) time" do
+        dom_proc = method(:dominant_octopus)
+        expect(n_log_n_runtime_with_sample?(100, &dom_proc)).to be(true)
       end
     end
 
     context "for a thousand fishes" do
       it "finds the longest fish in O(nlog(n)) time" do
-        thousands_of_fishes = get_fishes(1000)
-
-        start_time = Time.now
-        dummy = "0"
-        thousands_of_fishes.each { |fish| fish == dummy }
-        end_time = Time.now
-
-        c = 2
-        linear_run_time = c * (end_time - start_time)
-        
-        start_time = Time.now
-        clever_octopus(thousands_of_fishes)
-        end_time = Time.now
-        clever_run_time = end_time - start_time
-
-        expect(clever_run_time).to be < linear_run_time
+        dom_proc = method(:dominant_octopus)
+        expect(n_log_n_runtime_with_sample?(1000, &dom_proc)).to be(true)
       end
     end
+
   end
 
   describe "#sluggish_octopus" do
-    
+
+    it "finds the longest fish" do
+      expect(sluggish_octopus(fishes)).to eq('fiiiissshhhhhh')
+    end
+    context "for 10 fishes" do
+      it "finds the longest fish in O(n^2) time" do
+        slug_proc = method(:sluggish_octopus)
+        expect(n_n_runtime_with_sample?(10, &slug_proc)).to be(true)
+      end
+    end
+
+    context "for a hundred fishes" do
+      it "finds the longest fish in O(n^2) time" do
+        slug_proc = method(:sluggish_octopus)
+        expect(n_n_runtime_with_sample?(100, &slug_proc)).to be(true)
+      end
+    end
+
+    context "for a thousand fishes" do
+      it "finds the longest fish in O(n^2) time" do
+        slug_proc = method(:sluggish_octopus)
+        expect(n_n_runtime_with_sample?(1000, &slug_proc)).to be(true)
+      end
+    end
 
   end
 
