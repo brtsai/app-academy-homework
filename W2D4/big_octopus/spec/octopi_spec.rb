0ef255c0c8cccbb857ebd5f6a94212a794baf74f
brtsai <brtsai@ucsc.edu>
2018-02-21 19:31:54 -0800

    added rspec testing for O(n) time

--- W2D4/big_octopus/spec/octopi_spec.rb
+++ W2D4/big_octopus/spec/octopi_spec.rb
@@ -15,18 +15,25 @@ def get_fishes(amount)
 end
 
 def simulate_o_n_time(n)
-  hundred_fishes = get_fishes(100)
+  fishes = get_fishes(n)
+
   start_time = Time.now
-  dummy = "0"
-  hundred_fishes.each { |fish| fish == dummy }
+  fishes.each { |fish| fish.length == fish.length }
   end_time = Time.now
   
   c = 2
   c * (end_time - start_time)
 end
 
+def linear_runtime_with_sample?(n, &prc)
+  linear_run_time = simulate_o_n_time(n)
+  proc_run_time = runtime_with_n_fishes(n, &prc)
+  proc_run_time < linear_run_time
+end
+
 def runtime_with_n_fishes(n, &prc)
   fishes = get_fishes(n)
+
   start_time = Time.now
   prc.call(fishes)
   end_time = Time.now
@@ -38,37 +45,29 @@ end
 
 describe "Octopi" do
   let (:fishes) { ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'] }
-  
   describe "#clever_octopus" do
     it "finds the longest fish" do
       expect(clever_octopus(fishes)).to eq('fiiiissshhhhhh')
     end
+    
     context "for 10 fishes" do
       it "finds the longest fish in O(n) time" do
-        linear_run_time = simulate_o_n_time(10)
         clever_proc = method(:clever_octopus)
-        clever_run_time = runtime_with_n_fishes(10, &clever_proc)
-        expect(clever_run_time).to be < linear_run_time
+        expect(linear_runtime_with_sample?(10, &clever_proc)).to be(true)
       end
     end
 
     context "for a hundred fishes" do
       it "finds the longest fish in O(n) time" do
-        linear_run_time = simulate_o_n_time(100)
-        expect(clever_run_time).to be < linear_run_time
+        clever_proc = method(:clever_octopus)
+        expect(linear_runtime_with_sample?(100, &clever_proc)).to be(true)
       end
     end
 
     context "for a thousand fishes" do
       it "finds the longest fish in O(n) time" do
-        linear_run_time = simulate_o_n_time(1000)
-        
-        start_time = Time.now
-        clever_octopus(thousands_of_fishes)
-        end_time = Time.now
-        clever_run_time = end_time - start_time
-
-        expect(clever_run_time).to be < linear_run_time
+        clever_proc = method(:clever_octopus)
+        expect(linear_runtime_with_sample?(1000, &clever_proc)).to be(true)
       end
     end
   end
@@ -76,7 +75,7 @@ describe "Octopi" do
   describe "#dominant_octopus" do
     
     it "finds the longest fish" do
-      expect(clever_octopus(fishes)).to eq('fiiiissshhhhhh')
+      expect(dominant_octopus(fishes)).to eq('fiiiissshhhhhh')
     end
     context "for a small set of fishes" do
       it "finds the longest fish in O(n) time" do
