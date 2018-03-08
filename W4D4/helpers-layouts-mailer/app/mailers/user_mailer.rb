125d0aa7b43fc3ef39d4489b24a8c85d0d5ceb33
brtsai <brtsai@ucsc.edu>
2018-03-07 23:37:56 -0800

    implement welcome emailing for new users

--- W4D4/helpers-layouts-mailer/app/mailers/user_mailer.rb
+++ W4D4/helpers-layouts-mailer/app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
-class ApplicationMailer < ActionMailer::Base
+class UserMailer < ActionMailer::Base
   default from: 'every@appacademy.io'
   layout 'mailer'
 
   def welcome_email(user)
     @user = user
     @url  = 'http://www.99cats.com/login'
-    mail(to: user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: "#{@user.username}@appacademy.io", subject: 'Welcome to My Awesome Site')
   end
 end
