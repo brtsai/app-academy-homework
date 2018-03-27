1a8544d79a205b98c3a54886cfb6a5bd59a69d47
brtsai <brtsai@ucsc.edu>
2018-03-26 23:44:35 -0700

    addMoreLogging middleware for the bonus to observe the role of middlewares in the redux cycle

--- W7D2/middleware/frontend/store/store.js
+++ W7D2/middleware/frontend/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch, addMoreLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
@@ -12,8 +12,14 @@ const configureStore = (preloadedState = {}) => {
 const addLoggingToDispatch = store => next => action => {
     console.log(store.getState());
     console.log(action);
+    console.log(next);
     next(action);
     console.log(store.getState());
 };
+
+const addMoreLoggingToDispatch = store => next => action => {
+  console.log(next);
+  next(action);
+}
 
 export default configureStore;
