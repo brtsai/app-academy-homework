eec98490e532e80a15d70b5f83967e15783eeaa9
brtsai <brtsai@ucsc.edu>
2018-03-26 23:42:29 -0700

    move logging middleware from entry file to store.js refactor to use redux's applyMiddleware over handrolled applyMiddlewares

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -2,31 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, addLoggingToDispatch);
+  const store = configureStore(preloadedState);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
-const addLoggingToDispatch = store => next => action => {
-  // your code here
-    console.log(store.getState());
-    console.log(action);
-    next(action);
-    console.log(store.getState());
-};
-
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  for(let i = 0; i < middlewares.length; i++) {
-    dispatch = middlewares[i](store)(dispatch);
-  }
-
-  return Object.assign({}, store, { dispatch });
-};
